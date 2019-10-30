ea330083b3ef21e0266c8d5218e32965af8d5475
Randy Steele <steelerx155@gmail.com>
2019-10-30 19:12:22 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -46,6 +46,14 @@ def position_taken?(index_i)
   def valid_move?(index)
     index.between?(0,8) && !position_taken?(index)
   end
+  def turn_count
+    number_of_turns = 0
+    @board.each do |space|
+    if space == "X" || space == "O"
+        number_of_turns += 1
+    end
+  end
+  return number_of_turns
   
 end  
   
