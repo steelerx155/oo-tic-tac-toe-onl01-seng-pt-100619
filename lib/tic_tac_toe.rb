86d249f67c859597531d4b75f3c20883cac0bc5a
Randy Steele <steelerx155@gmail.com>
2019-10-30 19:01:19 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -70,6 +70,17 @@ def input_to_index(user_input)
     else
       turn
     end
+    
+    def won?
+  WIN_COMBINATIONS.detect do |win_combo|
+    if (@board[win_combo[0]]) == "X" && (@board[win_combo[1]]) == "X" && (@board[win_combo[2]]) == "X"
+      return win_combo
+    elsif (@board[win_combo[0]]) == "O" && (@board[win_combo[1]]) == "O" && (@board[win_combo[2]]) == "O"
+      return win_combo
+    end
+      false
+  end
+end
 end
   
 end  
