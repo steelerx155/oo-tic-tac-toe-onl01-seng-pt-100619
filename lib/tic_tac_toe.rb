8b39f34dabed8064d0a0259f2da771475ea75bc8
Randy Steele <steelerx155@gmail.com>
2019-10-30 19:14:41 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -110,6 +110,16 @@ def winner
   end
 end
 end
+def play
+  while over? == false
+    turn
+  end
+  if won?
+    puts "Congratulations #{winner}!"
+  elsif draw?
+    puts "Cat's Game!"
+  end
+end
 end 
 
   
