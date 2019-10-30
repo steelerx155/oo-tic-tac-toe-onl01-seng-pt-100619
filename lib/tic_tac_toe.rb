d81e2555f5e011d6b81f47538f018361c13ef08a
Randy Steele <steelerx155@gmail.com>
2019-10-30 02:20:09 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,21 +1,23 @@
+require_relative '../lib/tic_tac_toe.rb'
 class TicTacToe
   
 attr_accessor  :board, :WIN_COMBINATIONS
 
+ WIN_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2]
+  ]
+
 def initialize
-  @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-  @WIN_COMBINATIONS = [
-    ["0,1,2"],
-    ["3,4,5"],
-    ["6,7,8"],
-    ["0,3,6"],
-    ["1,4,7"],
-    ["2,5,8"],
-    ["0,4,8"],
-    ["6,4,2"]
-    ]
-  end
-end  
+  @board = Array.new(9, " ")
+ end
+end
  
  
    
