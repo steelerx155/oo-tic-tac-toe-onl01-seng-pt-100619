c87ad4bc9158946a40f3412303d7c58bddd8b231
Randy Steele <steelerx155@gmail.com>
2019-10-30 00:05:55 +0000

    Done.

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,17 @@
+class TicTacToe
+  
+attr_accessor  :board, :WIN_COMBINATIONS
+
+def initialize
+  @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  @WIN_COMBINATIONS = [
+    ["0,1,2"],
+    ["3,4,5"]
+    ]
+  end
+end  
+ 
+ 
+   
+
+
