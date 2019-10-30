3e13ee2936fdd40a2e5a86ad3ad1176a0de41e71
Randy Steele <steelerx155@gmail.com>
2019-10-30 19:09:15 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,6 +1,34 @@
 require_relative '../lib/tic_tac_toe.rb'
 
+# require_relative '../lib/tic_tac_toe.rb'
+class TicTacToe
+  
+attr_accessor  :board, :WIN_COMBINATIONS, :display_board
 
+ WIN_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2]
+  ]
+
+def initialize
+  @board = Array.new(9, " ")
+ 
+end
+
+def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+ end
   
    def input_to_index(user_input)
     user_input.to_i - 1
