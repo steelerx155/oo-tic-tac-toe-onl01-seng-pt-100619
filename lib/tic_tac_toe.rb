e55d4f2f858b5e6a6a4f838eddee1eafbd21a251
Randy Steele <steelerx155@gmail.com>
2019-10-30 19:04:34 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,40 +1,48 @@
-require_relative '../lib/tic_tac_toe.rb'
+
 class TicTacToe
-  
-attr_accessor  :board, :WIN_COMBINATIONS, :display_board
+  def initialize(board = nil)
+    @board = board || Array.new(9, " ")
+  end
+end 
 
- WIN_COMBINATIONS = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [6, 4, 2]
+# The "pipes" || = or. This is saying that if the board returns nil, return an empty array.  So, either display the current board, or a new one.
+
+# WIN_COMBINATIONS within the body of TicTacToe
+
+WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
   ]
-
-def initialize
-  @board = Array.new(9, " ")
- 
-end
-
-def display_board
+  
+  #display_board: not the instance variable @board
+  
+    def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
     puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
     puts "-----------"
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
-
-def input_to_index(user_input)
+  
+  # input_index
+  
+  def input_to_index(user_input)
     user_input.to_i - 1
   end
+  
+  # move: Note that we deleted the boar arguement, and added @ to board.  For instance, #move was move(board, position, char), but now board is intialized, so it is a characteristic of TicTacToe, no need to have it as an argument.  So, #move became simply move(position, char).
+  
   
   def move(position, char)
     @board[position] = char
   end
-  
+# For #move to work, we need to position_taken and valid_move
   def position_taken?(index_i)
     ((@board[index_i] == "X") || (@board[index_i] == "O"))
   end
@@ -43,22 +51,27 @@ def input_to_index(user_input)
     index.between?(0,8) && !position_taken?(index)
   end
   
- def turn_count
+
+#turn_count
+  def turn_count
     number_of_turns = 0
     @board.each do |space|
     if space == "X" || space == "O"
         number_of_turns += 1
+    end
   end
   return number_of_turns
   
-   def current_player
+#current_player
+  def current_player
     if turn_count % 2 == 0
     "X"
     else
     "O"
   end
-  
-  def turn
+ 
+#turn 
+def turn
     puts "Please enter 1-9:"
     input = gets.strip
     index = input_to_index(input)
@@ -69,9 +82,10 @@ def input_to_index(user_input)
     else
       turn
     end
-  end
-    
-    def won?
+end
+
+#won?
+def won?
   WIN_COMBINATIONS.detect do |win_combo|
     if (@board[win_combo[0]]) == "X" && (@board[win_combo[1]]) == "X" && (@board[win_combo[2]]) == "X"
       return win_combo
@@ -79,7 +93,7 @@ def input_to_index(user_input)
       return win_combo
     end
       false
-   end
+  end
 end
 
 #full?
