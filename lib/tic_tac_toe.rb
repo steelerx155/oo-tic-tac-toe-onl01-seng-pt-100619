5324bb0e55ec384e50e7b1d5924af6caf86b6ff4
Randy Steele <steelerx155@gmail.com>
2019-10-30 19:13:34 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -65,7 +65,18 @@ def current_player
   
  end 
  
- 
+ def turn
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    char = current_player
+    if valid_move?(index)
+      move(index, char)
+      display_board
+    else
+      turn
+    end
+end
  
  
  
